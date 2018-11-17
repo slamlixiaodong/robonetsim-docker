ac12216dcc5e45f523cbb0f14f1fa9885c26ab17
Tetsuya Idota <tidota@hawaii.edu>
2018-11-16 22:13:08 -1000

    First Commit

---
+++ run.bash
@@ -0,0 +1,76 @@
+#!/usr/bin/env bash
+
+# Runs a docker container with the image created by build.bash
+# Requires:
+#   docker
+#   nvidia-docker
+#   an X server
+# Recommended:
+#   A joystick mounted to /dev/input/js0 or /dev/input/js1
+
+if [ $# -lt 1 ]
+then
+    echo "Usage: $0 <docker image> [<dir with workspace> ...]"
+    exit 1
+fi
+
+IMG=$(basename $1)
+
+ARGS=("$@")
+WORKSPACES=("${ARGS[@]:1}")
+
+# Make sure processes in the container can connect to the x server
+# Necessary so gazebo can create a context for OpenGL rendering (even headless)
+XAUTH=/tmp/.docker.xauth
+if [ ! -f $XAUTH ]
+then
+    xauth_list=$(xauth nlist :0 | sed -e 's/^..../ffff/')
+    if [ ! -z "$xauth_list" ]
+    then
+        echo $xauth_list | xauth -f $XAUTH nmerge -
+    else
+        touch $XAUTH
+    fi
+    chmod a+r $XAUTH
+fi
+
+DOCKER_OPTS=
+
+# Share your vim settings.
+VIMRC=~/.vimrc
+if [ -f $VIMRC ]
+then
+  DOCKER_OPTS="$DOCKER_OPTS -v $VIMRC:/home/developer/.vimrc:ro"
+fi
+
+for WS_DIR in ${WORKSPACES[@]}
+do
+  WS_DIRNAME=$(basename $WS_DIR)
+  if [ ! -d $WS_DIR/src ]
+  then
+    echo "Other! $WS_DIR"
+    DOCKER_OPTS="$DOCKER_OPTS -v $WS_DIR:/home/developer/other/$WS_DIRNAME"
+  else
+    echo "Workspace! $WS_DIR"
+    DOCKER_OPTS="$DOCKER_OPTS -v $WS_DIR:/home/developer/workspaces/$WS_DIRNAME"
+  fi
+done
+
+# Mount extra volumes if needed.
+# E.g.:
+# -v "/opt/sublime_text:/opt/sublime_text" \
+
+docker run -it \
+  -e DISPLAY \
+  -e QT_X11_NO_MITSHM=1 \
+  -e XAUTHORITY=$XAUTH \
+  -v "$XAUTH:$XAUTH" \
+  -v "/tmp/.X11-unix:/tmp/.X11-unix" \
+  -v "/etc/localtime:/etc/localtime:ro" \
+  -v "/dev/input:/dev/input" \
+  --privileged \
+  --rm \
+  --runtime=nvidia \
+  --security-opt seccomp=unconfined \
+  $DOCKER_OPTS \
+  $IMG
